26f770b0c6896c3081eb668df1df00e6130a560d
CosmicCoder96 <abhinav.khare31@gmail.com>
2016-08-23 00:52:32 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,31 @@
   });
 
 }).call(this);
+
+var flag=0;
+  $("#circle-menu").click(function(){
+  	if(flag==0){
+  		$('.test').css({
+  	
+   'filter'         : 'blur(5px)',
+   '-webkit-filter' : 'blur(5px)',
+   '-moz-filter'    : 'blur(5px)',
+   '-o-filter'      : 'blur(5px)',
+   '-ms-filter'     : 'blur(5px)'
+});
+  		flag =1;}
+  		else
+  		{
+  				$('.test').css({
+  	
+   'filter'         : 'blur(0px)',
+   '-webkit-filter' : 'blur(0px)',
+   '-moz-filter'    : 'blur(0px)',
+   '-o-filter'      : 'blur(0px)',
+   '-ms-filter'     : 'blur(0px)'
+});
+  				flag=0;
+  		}
+
+
+	});
